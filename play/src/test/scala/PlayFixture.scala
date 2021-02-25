538f7e06951e122e602d25617e9dbe22c419a3b1
Erwan Loisant <erwan@zendesk.com>
2021-02-25 21:50:26 +0100

    Introduce Scalafix

--- play/src/test/scala/PlayFixture.scala
+++ play/src/test/scala/PlayFixture.scala
@@ -9,7 +9,6 @@ import play.api.mvc.Results._
 import play.api.mvc._
 import play.api.test.Helpers._
 import play.api.test._
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 case class User(id: Long, name: String)
@@ -30,45 +29,59 @@ trait PlayFixture extends Fixture {
 
   // The expiration is not added in the same way, resulting in JSON properties not in the same order,
   // meaning a different Base64 encoding
-  val playClaim64 = "eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwiZXhwIjoxMzAwODE5MzgwfQ"
+  val playClaim64 =
+    "eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwiZXhwIjoxMzAwODE5MzgwfQ"
 
-  def loginAction(implicit conf:Configuration, Action:DefaultActionBuilder): EssentialAction = Action { implicit request =>
-    val body = request.body.asJson
-    val password = (body.get \ "password").as[String]
+  def loginAction(implicit conf: Configuration, Action: DefaultActionBuilder): EssentialAction =
+    Action { implicit request =>
+      val body = request.body.asJson
+      val password = (body.get \ "password").as[String]
 
-    password match {
-      case "p4ssw0rd" => Ok.withJwtSession(Json.obj("user" -> userJson))
-      case _ => BadRequest
+      password match {
+        case "p4ssw0rd" => Ok.withJwtSession(Json.obj("user" -> userJson))
+        case _          => BadRequest
+      }
     }
-  }
 
-  def logoutAction(implicit conf:Configuration, Action:DefaultActionBuilder): EssentialAction = Action {
-    Ok.withoutJwtSession
-  }
-
-  def classicAction(implicit conf:Configuration, Action:DefaultActionBuilder): EssentialAction = Action { implicit request =>
-    Ok.refreshJwtSession
-  }
-
-  def securedAction(implicit conf:Configuration, Action:DefaultActionBuilder): EssentialAction = Action { implicit request =>
-    request.jwtSession.getAs[User]("user") match {
-      case Some(u) => Ok.refreshJwtSession
-      case _ => Unauthorized.withoutJwtSession
+  def logoutAction(implicit conf: Configuration, Action: DefaultActionBuilder): EssentialAction =
+    Action {
+      Ok.withoutJwtSession
     }
-  }
 
-  def get(action: EssentialAction, header: Option[String] = None)(implicit conf:Configuration, mat:Materializer, Action:DefaultActionBuilder) = {
-    var request = header match {
-      case Some(h) => FakeRequest(GET, "/something").withHeaders((JwtSession.REQUEST_HEADER_NAME, h))
+  def classicAction(implicit conf: Configuration, Action: DefaultActionBuilder): EssentialAction =
+    Action { implicit request =>
+      Ok.refreshJwtSession
+    }
+
+  def securedAction(implicit conf: Configuration, Action: DefaultActionBuilder): EssentialAction =
+    Action { implicit request =>
+      request.jwtSession.getAs[User]("user") match {
+        case Some(_) => Ok.refreshJwtSession
+        case _       => Unauthorized.withoutJwtSession
+      }
+    }
+
+  def get(action: EssentialAction, header: Option[String] = None)(implicit
+      conf: Configuration,
+      mat: Materializer
+  ) = {
+    val request = header match {
+      case Some(h) =>
+        FakeRequest(GET, "/something").withHeaders((JwtSession.REQUEST_HEADER_NAME, h))
       case _ => FakeRequest(GET, "/something")
     }
 
     call(action, request)
   }
 
-  def post(action: EssentialAction, body: JsObject)(implicit mat:Materializer, Action:DefaultActionBuilder) = {
+  def post(action: EssentialAction, body: JsObject)(implicit
+      mat: Materializer
+  ) = {
     call(action, FakeRequest(POST, "/something").withJsonBody(body))
   }
 
-  def jwtHeader(of: Future[Result])(implicit timeout: Timeout, conf:Configuration): Option[String] = header(JwtSession.RESPONSE_HEADER_NAME, of)(timeout)
+  def jwtHeader(
+      of: Future[Result]
+  )(implicit timeout: Timeout, conf: Configuration): Option[String] =
+    header(JwtSession.RESPONSE_HEADER_NAME, of)(timeout)
 }
